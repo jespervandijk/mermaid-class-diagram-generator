d9cc608b7ff4821db5aa7e8fa15bda69e7e55522
jespervandijk <jespervandijk@icloud.com>
2024-11-19 12:31:08 +0100

    only add user defined types tests

---
+++ Test/OnlyAddUserDefinedTypesTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using MermaidClassDiagramGenerator;
+
+namespace Test;
+
+public class OnlyAddUserDefinedTypesTest
+{
+    private const string FilePath = "../../../Outputs/onlyAddUserDefinedTypes.md";
+    
+    private class Car : Object
+    {
+    }
+
+    [Fact]
+    public void Generate_ClassInheritsFromDotnetType_DotnetTypeNotDocumented()
+    {
+        var generator = new DiagramGenerator(FilePath, [Assembly.GetExecutingAssembly()],
+            [typeof(Car)], true);
+        generator.Generate();
+        var content = File.ReadAllText(FilePath);
+        content.Should().Contain("class Car");
+        content.Should().NotContain("class Object");
+    }
+    
+    [Fact]
+    public void Generate_ClassInheritsFromDotnetTypeAndDotnetTypeDirectlyPassed_DotnetTypeDocumented()
+    {
+        var generator = new DiagramGenerator(FilePath, [Assembly.GetExecutingAssembly()],
+            [typeof(Car), typeof(Object)], true);
+        generator.Generate();
+        var content = File.ReadAllText(FilePath);
+        content.Should().Contain("class Car");
+        content.Should().Contain("class Object");
+    }
+}
